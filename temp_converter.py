d77e6cea5456c81c083a01f162eaf8f88b174195
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-09-25 11:51:59 +0800

    Ask the user to input temperature and added changes in conversion_type

--- temp_converter.py
+++ temp_converter.py
@@ -1,4 +1,5 @@
 # 1. Ask user first if Celsius to Fahrenheit or Fahrenheit to Celsius
-conversion_type = input("What conversion do you want to use?\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius ")
+conversion_type = input("What conversion do you want to use?\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius\nEnter your answer: ")
 # 2. Ask the user to input temperature
+temp_input = float(input("Enter the temperature: "))
 # 3. Convert and print result
