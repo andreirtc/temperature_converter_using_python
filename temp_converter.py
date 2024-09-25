6daeb6ded5451318ece7196778c55766dab1e75b
Ralph Andrei Castillo <ralphandreicastillo326@gmail.com>
2024-09-25 11:46:33 +0800

    Ask user first if Celsius to Fahrenheit or Fahrenheit to Celsius

--- temp_converter.py
+++ temp_converter.py
@@ -1,3 +1,4 @@
 # 1. Ask user first if Celsius to Fahrenheit or Fahrenheit to Celsius
+conversion_type = input("What conversion do you want to use?\n1. Celsius to Fahrenheit\n2. Fahrenheit to Celsius ")
 # 2. Ask the user to input temperature
 # 3. Convert and print result
